77a72536ba0e5c98fa3d0956ec17849976d91893
izamfirache <izamfirache@isoftwaresystems.com>
2019-01-30 11:21:11 +0200

    test source code project example added

---
+++ ATOOSTestSourceCodeProject1/Client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATOOSTestSourceCodeProject1
+{
+    public class Client
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public IEmployee AssociatedEmployee;
+
+        public Client(string name, string surname, int age, IEmployee associatedEmployee)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            AssociatedEmployee = associatedEmployee;
+        }
+
+        public string GetClientInfo(string name, string surname, int age)
+        {
+            return string.Format("{0} {1}, {2} years of age.", name, surname, age);
+        }
+
+        public string GetAssociatedEmployeeInfo()
+        {
+            return string.Format("{0} {1}, {2} years of age.",
+                AssociatedEmployee.GetEmployeeName(),
+                AssociatedEmployee.GetEmployeeSurname(),
+                AssociatedEmployee.GetEmployeeAge());
+        }
+
+        public string GetClientName()
+        {
+            return Name;
+        }
+
+        public string GetClientSurname()
+        {
+            return Surname;
+        }
+
+        public int GetClientSalary(Client client)
+        {
+            if (client.Age > 10)
+            {
+                return 1000;
+            }
+            else
+            {
+                return 2000;
+            }
+        }
+
+        public string ReturnNullForTestingPurpose()
+        {
+            return null;
+        }
+    }
+}
---
+++ ATOOSTestSourceCodeProject1/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿namespace ATOOSTestSourceCodeProject1
+{
+    public interface IEmployee
+    {
+        string GetEmployeeInfo(string name, string surname, int age);
+
+        string GetEmployeeName();
+
+        string GetEmployeeSurname();
+        int GetEmployeeAge();
+
+        int GetEmployeeSalary(Employee employee);
+
+        string ReturnNullForTestingPurpose();
+    }
+}
